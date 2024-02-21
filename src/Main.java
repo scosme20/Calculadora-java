d8596b3942a25677110a792f180c68c66ed0124b
scosme20 <scosmeagostinho@hotmail.com>
2024-02-21 19:26:19 -0300

    Calculadora

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,44 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        System.out.println("Olá mundo!");
-        System.out.println("Olá eu sou java");
+class javaCalculator
+{
+
+    public static void main(String[] args)
+    {
+        int num1;
+        int num2;
+        String operation;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Insira o primeiro número");
+        num1 = input.nextInt();
+
+        System.out.println("Insira o segundo número");
+        num2 = input.nextInt();
+
+        Scanner op = new Scanner(System.in);
+
+        System.out.println("selecione a operação");
+        operation = op.next();
+
+        if (operation.equals("+"))
+        {
+            System.out.println("Sua resposta é " + (num1 + num2));
+        }
+        if (operation.equals("-"))
+        {
+            System.out.println("Sua resposta é " + (num1 - num2));
+        }
+
+        if (operation.equals("/"))
+        {
+            System.out.println("Sua resposta é " + (num1 / num2));
+        }
+        if (operation.equals("*"))
+        {
+            System.out.println("Sua resposta é " + (num1 * num2));
+        }
 
     }
 }
